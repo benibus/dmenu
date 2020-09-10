66598e76ff23d22c1db85f3a40dac978c76635a3
benibus <bpharks@gmx.com>
2020-09-10 14:04:40 -0400

    Init config

--- config.h
+++ config.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"DroidSansMono Nerd Font:size=9:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*               fg         bg        */
+	[SchemeNorm] = { "#B0B9C0", "#0C0D0E" },
+	[SchemeSel] =  { "#BCC7CE", "#25292A" },
+	[SchemeOut] =  { "#05050A", "#4B5867" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
